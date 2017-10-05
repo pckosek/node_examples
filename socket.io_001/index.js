3bd61be184f5155103cdd31a6d02248a880935be
pckosek <pckosek@local.tjhsst.edu>
2017-10-05 08:10:12 -0400

    added comments

--- socket.io_001/index.js
+++ socket.io_001/index.js
@@ -1,23 +1,30 @@
 #!/usr/bin/nodejs
 
+// INITIALIZATION STUFF
 var express = require('express'),
 http = require('http');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+// PORT SETUP - NUMBER SPECIFIC TO THIS SYSTEM
 server.listen(process.env.PORT || 8080);
 
+// WHAT TO DO WHEN A CONNECTION IS ESTABLISHED
 io.on('connection',function(socket){
+    
+    // here, I log to the server
     console.log("a connection!");
+
+    // I write a callback as to what to do when a message named testMessage arrives 
     socket.on("testMessage", function(data){
-        console.log(data);
-        socket.emit('foo', {data: 1} );
+        socket.emit('foo', {data: data+1} );
+        console.log('testMessage with data:' + data);
     })
 })
 
-// GET, POST
 
+// EXPRESS GETTER FOR PAGES
 app.get('/', function (req, res, next) {
     res.sendFile(__dirname+'/index.html');
 });
