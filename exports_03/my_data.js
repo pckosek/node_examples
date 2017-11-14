d3c071af5225142df1400e9f850e34f1d26d2f30
pckosek <pckosek@local.tjhsst.edu>
2017-11-14 13:27:59 -0500

    comments

--- exports_03/my_data.js
+++ exports_03/my_data.js
@@ -1,11 +1,14 @@
+ // Define some function
 function doMethodA() {
 	console.log('method A');
 }
 
+ // Define another function
 function doMethodB() {
 	console.log('method B');
 }
 
+// create a module exports element, and add our functions fo the exports
 module.exports = {};
 module.exports['doMethodA'] = doMethodA;
 module.exports['doMethodB'] = doMethodB;
