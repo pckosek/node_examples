f0edf5547d2a54d176103ba6d3d613be2a6f4733
pckosek <pckosek@local.tjhsst.edu>
2018-02-06 08:54:47 -0500

    four

---
+++ _javascript_intro_4/README.md
@@ -0,0 +1,9 @@
+## Node Server with console logging
+Installation instructions are as before
+
+### The intent of this server is to demonstrate console logging.
+1) open a new tab with the console.
+2) cd into the directory that contains your log file
+3) run the command `tail -f info.log` to see live updates of your log file
+
+**You will see logs appear as your server starts up, and any time a user lands at not_a_search.**
---
+++ _javascript_intro_4/index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/nodejs
+
+// // -------------- load packages -------------- //
+
+// INITIALIZATION STUFF
+var express = require('express')
+var app = express();
+
+console.log('SYSTEM STARTING UP...')
+// // -------------- express initialization -------------- //
+// PORT SETUP - NUMBER SPECIFIC TO THIS SYSTEM
+app.set('port', process.env.PORT || 8080 );
+
+
+console.log('REGISTERING GET HANDLERS...')
+// // -------------- express 'get' handlers -------------- //
+// // These 'getters' are what fetch your pages
+
+app.get('/', function(req, res){
+    res.send('hola');
+});
+
+app.get('/foo', function(req, res){
+    res.send('requested foo');
+});
+
+app.get('/not_a_search', function(req, res){
+    var theQuery = req.query.q;
+    res.send('query q parameter:' + theQuery);
+    
+    // log the parameter
+    console.log(theQuery);
+});
+
+
+
+console.log('CREATING LISTENTER...')
+// -------------- listener -------------- //
+// // The listener is what keeps node 'alive.' 
+
+var listener = app.listen(app.get('port'), function() {
+  console.log( 'Express server started on port: '+listener.address().port );
+});
