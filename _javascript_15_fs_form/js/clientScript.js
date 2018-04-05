1d3b949c5857c34e2f6a3774c88521a8a8f75497
pckosek <pckosek@local.tjhsst.edu>
2018-04-05 14:04:10 -0400

    jqueryform

--- _javascript_15_fs_form/js/clientScript.js
+++ _javascript_15_fs_form/js/clientScript.js
@@ -1,6 +1,8 @@
 // HOW TO GET THE VALUE FROM THE FORM
 
 function checkBallForm() {
+    // This is a simple example of form validation.
+
     ball_form_element = document.getElementById('ball_form');
     
     if (ball_form_element.shape.value==="") {
@@ -14,32 +16,30 @@ function checkBallForm() {
 // GET THE USER INFO FROM SUBMIT
 
 function usernameSubmitForm () {
+    // get the form as a dom js element. Used for the validation.
     user_form_element = document.getElementById('user_form');
+
+    // get the form as a jquery element. This is almost the same as above - but 
+    // it is easier to serialize down in the ajax request.
     jquser_form_element = $('#user_form');
-    valid = false;
     
+    // VALIDATE FORM
+    // - RETURN (exit function) IF THE FORM IS INCOMPLETE
     if (user_form_element.username.value==="") {
         console.log('no username present')
-        return valid;
+        return false;
     } else if (user_form_element.password.value==="") {
         console.log('no password present')
-        return valid;
-    } else {
-        valid = true;
-    }
+        return false;
+    } 
         
-    if (valid) {
-        $.ajax({
-            url: "submit_userpass",
-            type: "GET",
-            data: $(jquser_form_element).serialize(),
-
-            success: function(response) {
-                console.log(response);
-            }            
-        });        
+    $.ajax({
+        url: "submit_userpass",
+        type: "GET",
+        data: $(jquser_form_element).serialize(),
+        success: function(response) {
+            console.log(response);
+        }            
+    });        
             
-        
-    }
-
 }
