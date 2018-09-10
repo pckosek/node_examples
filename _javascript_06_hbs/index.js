7b249a2731791ad3d1760d5ba534ddde5c45d466
pckosek <pckosek@fcps.edu>
2018-09-10 08:20:23 -0400

    renumber

---
+++ _javascript_06_hbs/index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/nodejs
+
+
+// -------------- load packages -------------- //
+var express = require('express')
+var app = express();
+
+var hbs = require('hbs');
+
+
+// -------------- express initialization -------------- //
+app.set('port', process.env.PORT || 8080 );
+
+// tell express that the view engine is hbs
+app.set('view engine', 'hbs');
+
+
+// -------------- variable definition -------------- //
+var visitorCount = 0; 
+
+
+// -------------- express 'get' handlers -------------- //
+
+app.get('/', function(req, res){
+
+    // increment the number of visitors
+    visitorCount++;     
+    
+    // render the page
+    res.render('index', { numVisitors : visitorCount} );
+   
+
+    // ---- ALTERNATIVELY ----
+
+    // render the page
+    // res.render('index', 
+    // 	{ numVisitors : visitorCount}
+    // );
+
+
+    // ---- ALTERNATIVELY ----
+
+    // // dictionary of values
+    // var feed_dict = { numVisitors : visitorCount};
+
+    // // render the page
+    // res.render('index', feed_dict );
+
+});
+
+
+// -------------- listener -------------- //
+// The listener is what keeps node 'alive.' 
+
+var listener = app.listen(app.get('port'), function() {
+  console.log( 'Express server started on port: '+listener.address().port );
+});
