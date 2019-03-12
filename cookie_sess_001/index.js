525108198ee1b6ddcf06fa6f84d020608f1c715c
pckosek <pckosek@fcps.edu>
2019-03-12 13:48:54 -0400

    cookies

--- cookie_sess_001/index.js
+++ cookie_sess_001/index.js
@@ -1,42 +1,59 @@
 #!/usr/bin/nodejs
 
-
-// -------------- initialization stuff -------------- //
-
-var cookieSession = require('cookie-session')
+// -------------- load packages -------------- //
 var express = require('express')
 var app = express();
 
-// -- set the port for our system -- //
+var cookieSession = require('cookie-session')
+
+
+// -------------- express initialization -------------- //
 app.set('port', process.env.PORT || 8080 );
 
 
 // -------------- cookie configuration -------------- //
  
-app.set('trust proxy', 1) 
 app.use(cookieSession({
-  name: 'session',
-  keys: ['secret2', 'secret1']
+  name: 'snorkles',                         // ==> the name of the cookie is snorkles      
+  keys: ['enc_key_01_blah', 'enkey2_foo']   // ==> these two keys encrypt the cookie. CHANGE THEM! 
 }))
  
-// -------------- express getter -------------- //
+// -------------- express endpoints -------------- //
 
-app.get('/', function (req, res, next) {
-  //    (req.session.views || 0) => create 'req.session.views' if necessary
-  //    ..) + 1 => add one to it each time someone arrives
-  req.session.views = (req.session.views || 0) + 1
-  req.session.viewers = (req.session.viewers || 0) + 2
+app.get('/', function (req, res) {
+
+  // req.session is the cookie. It defaults to an object that you can add  
+  //   key-value pairs to.  
+  // 
+  // req.session is 'silently' passed in as part of the request AND returned
+  //   back to the browser. The data in req.session is stored in the browser 
+  //   until the client makes another request.
+  //
+  // the contents of req.session are unique to each browser
+  // 
+  // req.session will live in the cookies of the browser with the name provided
+  //   (in this example, "snorkles") until the user deletes the cookie.
+  //
+  // you can programmatically clear the cookie by setting the cookie to null:
+  //   req.session = null;
+  
  
-  // Write response 
-  res.send(req.session.views + ' views<br>'  + req.session.viewers + ' viewers<br>');
+  if( typeof(req.session.views)=='undefined' ) {            // if the cookie has not been set
+      req.session.views = 1;                                //   set it to 1;
+  } else {                                                  // otherwise, 
+      req.session.views++;                                  //   increment its value
+  }
+
+  res.send("Welcome, visitor #" + req.session.views );
 })
 
 app.get('/reset', function (req, res, next) {
-  console.log(req);
-  console.log(req.session);
-  
-  res.send('reset');
+  req.session = null;                                       // programmatically deletes the cookie
+  res.send('cookie has been reset');
 })
+
+// -------------- assign listener -------------- //
+// The listener is what keeps node 'alive.' 
 
 var listener = app.listen(app.get('port'), function() {
   console.log( 'Express server started on port: '+listener.address().port );
