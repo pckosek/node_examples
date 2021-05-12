6b77cf9a272ca822bb6725149eedb67433e54f69
Kosek <PCKosek@fcps.edu>
2021-05-12 08:22:03 -0400

    fxed?

--- _javascript_21_mysql/example_09_another_server_example/foo.sql
+++ _javascript_21_mysql/example_09_another_server_example/foo.sql
@@ -1,8 +1,29 @@
 DROP TABLE IF EXISTS foods;
-CREATE TABLE foods(id INT, f_name VARCHAR(100), menu VARCHAR(100), PRIMARY KEY(id));
-INSERT INTO foods(id, f_name, menu) VALUE (1, 'Soup', 'appetizer');
-INSERT INTO foods(id, f_name, menu) VALUE (2, 'Eggrolls', 'appetizer');
-INSERT INTO foods(id, f_name, menu) VALUE (3, 'salad', 'side');
-INSERT INTO foods(id, f_name, menu) VALUE (4, 'Chicken', 'main');
-INSERT INTO foods(id, f_name, menu) VALUE (5, 'Tofu', 'main');
-INSERT INTO foods(id, f_name, menu) VALUE (6, 'Brownies', 'dessert');
+CREATE TABLE foods(id INT, f_name VARCHAR(100), menu VARCHAR(100), price DECIMAL(4,2), PRIMARY KEY(id));
+INSERT INTO foods(id, f_name, menu, price) VALUE (1, 'Soup', 'appetizer', 1.50);
+INSERT INTO foods(id, f_name, menu, price) VALUE (2, 'Eggrolls', 'appetizer', 2.75);
+INSERT INTO foods(id, f_name, menu, price) VALUE (3, 'salad', 'side', 3.99);
+INSERT INTO foods(id, f_name, menu, price) VALUE (4, 'Chicken', 'main', 8.50 );
+INSERT INTO foods(id, f_name, menu, price) VALUE (5, 'Tofu', 'main', 7.95);
+INSERT INTO foods(id, f_name, menu, price) VALUE (6, 'Brownies', 'dessert', 2.99);
+
+
+# drop existing visitors_proc
+DROP PROCEDURE IF EXISTS menu_proc; 
+ 
+DELIMITER $$ 
+ 
+# name the procedure; this one will have no arguments
+CREATE PROCEDURE menu_proc() 
+BEGIN
+
+SELECT * FROM foods WHERE menu='appetizer';
+SELECT * FROM foods WHERE menu='side';
+SELECT * FROM foods WHERE menu='main';
+SELECT * FROM foods WHERE menu='dessert';
+
+# statement (therefore, procedure) is over
+END$$ 
+
+# change the delimiter back to normal
+DELIMITER ; 
